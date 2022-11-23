d6fc6d8b5cd9637c6bcbe67ca740c5afb24cafe2
Alif Imanullah Krisna Alam <ajaalif94@gmail.com>
2022-11-23 22:09:25 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Skoolen | </title>
+    <title>Skoolen | Index</title>
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <style>
+        nav a .skoleen{
+            color: orange;
+            color: blue;
+        }
+        .skoolen-mid{
+            color: white;
+        }
+        .skoolen-mid:hover{
+            color: orange;
+            color: blue;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 px-4 px-lg-5">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+            <h2 class="m-0 skoleen">Skoolen</h2>
+        </a>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Carousel Start -->
+    <div class="container-fluid p-0">
+        <div class="owl-carousel header-carousel position-relative">
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid" src="img/buku.jpg" alt="">
+                <div class="carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-lg-8 text-center">
+                                <h1 class="display-3 animated slideInDown mb-4 skoolen-mid">Skoolen</h1>
+                                <p class="fs-5 text-white mb-4 pb-2">Web terbaik untuk pembelajaran anak Sekolah Dasar (SD)</p>
+                                <a href="login.php" class="btn btn-dark rounded-pill py-md-3 px-md-4 animated slideInLeft">Login</a>
+                                <a href="register.php" class="btn btn-dark rounded-pill py-md-3 px-md-4 animated slideInRight">Register</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Carousel End -->
+    <div class="footer container-fluid py-4 bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="text-center">
+                    &copy; <a class="border-bottom" href="#" style="color: blue;">Skoolen</a>, All Right Reserved.
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright End -->
+    <!-- JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,136 @@
+<?php 
+require('koneksi.php');
+session_start();
+
+if(isset($_POST['submit'])){
+    $user = $_POST['txt_username'];
+    $pass = $_POST['txt_pass'];
+
+    if(!empty(trim($user)) && !empty(trim($pass))){
+        $query = "SELECT * FROM user_detail WHERE username='$user'";
+        $result = mysqli_query($koneksi,$query);
+        $num = mysqli_num_rows($result);
+
+        while ($row = mysqli_fetch_array($result)){
+            $id = $row['id'];
+            $userVal = $row['user_username'];
+            $passVal =$row['user_password'];
+            $username = $row['user_fullname'];
+            $lvl = $row['level'];
+        }
+
+        if($num != 0){
+            if($userVal==$email && $passVal==$pass){
+                // header('Location: dashboard.php?user_fullname='.urlencode($username));
+                $_SESSION['id'] = $id;
+                $_SESSION['name'] = $username;
+                $_SESSION['level'] = $lvl;
+                header('Location:admin/home.php');
+            }else{
+                $error = 'user atau password salah!!';
+                header('Location:login.php');
+            }
+        }else{
+            $error = 'user tidak ditemukan!!';
+            header('Location:login.php');
+        }
+    }else{
+        $error = 'Data tidak boleh kosong!!';
+        echo $error;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Skoolen | Login</title>
+	<meta charset="UTF-8">
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!--===============================================================================================-->
+</head>
+<body style="background-color: #666666;">
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" action="login.php" method="POST">
+					<span class="login100-form-title p-b-43">
+						Masuk untuk Melanjutkan
+					</span>
+					<div class="mb-3">
+    				<label for="username" class="form-label">Username</label>
+    				<input type="text" class="form-control" id="username" aria-describedby="username" name="txt_username">
+    				<div id="username" class="form-text"></div>
+  					</div>
+					<div class="mb-3">
+    				<label for="password" class="form-label">Password</label>
+    				<input type="password" class="form-control" id="password" aria-describedby="password" name="txt_pass">
+    				<div id="password" class="form-text"></div>
+  					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" type="submit" name="submit">
+							LOGIN
+						</button>
+					</div>
+
+					<div class="text-center w-full p-t-3 p-b-32">
+						<a href="register.php">
+							Belum punya akun? Daftar
+						</a>
+					</div>
+				</form>
+				<div class="login100-more" style="background-image: url('images/1.png');">
+				<a href="index.php" class="btn btn-dark py-md-2 px-md-4">←</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
